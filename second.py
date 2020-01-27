98dee284b1aece40e82091d22c894b0164541250
Moandh81 <anis.dhouieb@gmail.com>
2020-01-27 19:22:35 +0100

    add second test

--- second.py
+++ second.py
@@ -3,6 +3,8 @@ from app import addition
 class TestAdditionMethod(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(5, addition(2,3))
+    def test_is_upper(self):
+        self.assertTrue("UPPER".isupper())
 
 
 if __name__ == '__main__':
